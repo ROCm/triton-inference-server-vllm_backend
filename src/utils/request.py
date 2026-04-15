585ed0f7c144f8b64d573bfb4970e30aa89b819f
linsun12 <linsun12@amd.com>
2026-04-15 12:53:22 -0400

    vllm 1.19 instead of 1.11 is used

--- src/utils/request.py
+++ src/utils/request.py
@@ -33,7 +33,7 @@ from typing import Callable, Dict, List, Optional
 import numpy as np
 import triton_python_backend_utils as pb_utils
 from PIL import Image
-from vllm.inputs.data import TokensPrompt
+from vllm.inputs import TokensPrompt
 from vllm.lora.request import LoRARequest
 from vllm.outputs import (
     EmbeddingOutput,
